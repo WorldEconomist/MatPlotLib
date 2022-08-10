bbe5fb10cd4845794ff724a89569a0b2b8eeac53
Дмитрий <korneevd123@gmail.com>
2022-08-10 23:01:07 +0300

    feat: created my first python script

--- MatPlotLib_Test2.py
+++ MatPlotLib_Test2.py
@@ -3,8 +3,13 @@ import matplotlib.pyplot as plt
 
 fig, axes = plt.subplots(2, 2)
 
-x = [-3, -2, -1, 0, 1, 2, 3]
-y = [9, 4, 1, 0, 1, 4, 9]
-
-axes[1, 0].plot(x,y)
+x = np.array([-3, -2, -1, 0, 1, 2, 3])
+y = np.array([9, 4, 1, 0, 1, 4, 9])
+axes[1, 0].plot(x, y, lw=0.5)
+axes[1, 1].plot(x, y, label='idi nahuy')
+axes[0, 0].plot(x + 4, y / 3, c='red', ls='-', label='y=x^2')
+axes[0, 0].set_xlim(2, 0)
+axes[1, 0].set_ylim(-5, 4)
+axes[0, 1].set_title('Tut snizu nichego net', fontsize=20)
+plt.legend(loc='lower left')
 plt.show()
