a41422670a35f5023370df568588ef72f835ac08
Дмитрий <korneevd123@gmail.com>
2022-08-21 22:41:29 +0300

    Just to practise

--- Simple_practice.py
+++ Simple_practice.py
@@ -8,12 +8,12 @@ z = x ** 2
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 ax2 = fig.add_subplot(222)
-ax1.plot(x,y)
-ax1.set_xlabel('x')
-ax1.set_ylabel('y')
+ax1.plot(x,z)
+ax1.set_xlabel('Axis X')
+ax1.set_ylabel('Axis Y')
 
 
 ax2.plot(x,y)
-ax2.set_xlabel('x')
-ax2.set_ylabel('y')
+ax2.set_xlabel('Axis X')
+ax2.set_ylabel('Axis Y')
 plt.show()
