e26c7ea83bca93c1543851ac6d54b1045c2d97b9
Дмитрий <korneevd123@gmail.com>
2022-09-11 21:06:43 +0300

    Big amount of GDP data

--- GDP.py
+++ GDP.py
@@ -13,25 +13,27 @@ USA = df[df['Country Code'] == 'USA']
 Russia = df[df['Country Code'] == 'RUS']
 China = df[df['Country Code'] == 'CHN']
 
-plt.plot(USA['Year'], USA['Value'] / 1_000_000_000_000,
+number_formatter = 1_000_000_000_000
+
+plt.plot(USA['Year'], USA['Value'] / number_formatter,
          label='USA GDP')
-plt.plot(Russia['Year'], Russia['Value'] / 1_000_000_000_000,
+plt.plot(Russia['Year'], Russia['Value'] / number_formatter,
          label='Russia GDP')
-plt.plot(China['Year'], China['Value'] / 1_000_000_000_000,
+plt.plot(China['Year'], China['Value'] / number_formatter,
          label='China GDP')
 
-plt.title('USA, China and Russia GDP, $t',fontsize=20, fontweight='bold')
+plt.title('USA, China and Russia GDP, $t', fontsize=20, fontweight='bold')
 plt.xlabel("Year")
 plt.ylabel("GDP in Trillions of USD")
 plt.xticks(range(1960, 2020, 10))
 plt.ylim(0)
 
 plt.text(x=.8, y=.72, s="""USA""",
-        transform=fig.transFigure, ha='left', fontsize=8, alpha=.9)
+         transform=fig.transFigure, ha='left', fontsize=8, alpha=.9)
 plt.text(x=.8, y=.44, s="""China""",
-        transform=fig.transFigure, ha='left', fontsize=8, alpha=.9)
+         transform=fig.transFigure, ha='left', fontsize=8, alpha=.9)
 plt.text(x=.83, y=.165, s="""Russia""",
-        transform=fig.transFigure, ha='left', fontsize=8, alpha=.9)
+         transform=fig.transFigure, ha='left', fontsize=8, alpha=.9)
 plt.legend(loc='best')
 plt.savefig('GDP.png')
 
